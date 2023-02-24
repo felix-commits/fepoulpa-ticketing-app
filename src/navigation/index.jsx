beb0020edd4b91cc9e02b8076cb3e5943f2d3d0f
felix-commits <fpoullet@mailo.com>
2023-02-24 19:13:59 +0100

    clerk auth

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,4 +1,5 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { EmailVerification } from '../features/EmailVerification'
 import { Login } from '../features/Login'
 
 const Stack = createNativeStackNavigator()
@@ -6,5 +7,6 @@ const Stack = createNativeStackNavigator()
 export const NativeNavigation = () => (
   <Stack.Navigator screenOptions={{ headerShown: false }}>
     <Stack.Screen name="Dring Marché !" component={Login} />
+    <Stack.Screen name="Email verification" component={EmailVerification} />
   </Stack.Navigator>
 )
